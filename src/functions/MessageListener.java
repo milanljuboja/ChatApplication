bb523b640d416831a7dad884a6d57694fb4c6aa1
milanljuboja <milanljuboja@hotmail.com>
2019-04-12 15:25:28 +0200

    Chat aplikacija

---
+++ src/functions/MessageListener.java
@@ -0,0 +1,5 @@
+package functions;
+
+public interface MessageListener {
+    public void onMessage(String fromLogin, String msgBody);
+}
---
+++ src/functions/ServerMain.java
@@ -0,0 +1,9 @@
+package functions;
+
+public class ServerMain {
+    public static void main(String[] args) {
+        int port = 8818;
+        Server server = new Server(port);
+        server.start();
+    }
+}
